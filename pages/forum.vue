777e5ea2c15c3d90fe01b59dde4ecd7bf64407f0
Jonathan Fuqua <jfuqua390@gmail.com>
2021-06-18 17:41:03 -0400

    Try out transition

--- pages/forum.vue
+++ pages/forum.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="container-fluid">
+    <transition name="home"></transition>
         <Header />
         <div class="body">This will be forums</div>
     </div>
     
 </template>
+
+<script>
+export default {
+    transition: 'home'
+}
+</script>
+
+<style scoped>
+    .home-enter-active, .home-leave-active { transition: opacity .5s; }
+    .home-enter, .home-leave-active { opacity: 0; }
+</style>
